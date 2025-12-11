0f553a624b80ae745e7e4d6f43783c71fb8c1375
owjs3901 <owjs3901@gmail.com>
2025-12-11 21:58:36 +0900

    Add output

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,4 @@
-import { debug, error, getBooleanInput, info, setFailed } from '@actions/core'
+import { error, getBooleanInput, info, setFailed } from '@actions/core'
 import { exec } from '@actions/exec'
 import { context } from '@actions/github'
 import { checkPastChangepacks } from './check-past-changepacks'
@@ -41,7 +41,7 @@ export async function run() {
             return changepack.nextVersion !== null
           }),
         )
-        debug(
+        console.log(
           `filteredPastChangepacks: ${JSON.stringify(filteredPastChangepacks, null, 2)}`,
         )
         if (
